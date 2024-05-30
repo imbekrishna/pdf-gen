6b9b54a2b6a2d0348d64006432d3da9d667ad2b5
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-05-30 22:03:30 +0530

    Updated Certificate layout

--- api/app.js
+++ api/app.js
@@ -1,22 +1,12 @@
 import express from "express";
-import path from "path";
-
-import { fileURLToPath } from "url";
-
+import cors from "cors";
 import pdfRouter from "../routers/pdf.router.js";
 import { errorHandler } from "../middlewares/error.middleware.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
-app.use(express.json());
-app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "..", "view"));
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(express.json());
 
 app.use("/pdf", pdfRouter);
 
--- services/createPdf.js
+++ services/createPdf.js
@@ -27,218 +27,108 @@ function createPdf() {
 
 /**
  *
- * @param {import('pdfkit').PDFDocument} doc Instance of PDFDocument of pdfkit
+ * @param {import('pdfkit')} doc Instance of PDFDocument of pdfkit
  * @param {string} name - Name to be printed on the certificate
  * @param {number} gpa - Percentage scored
  * @param {string} grade - Grade awared
  */
 function createPage(doc, name, percentage, grade) {
   // Header
-  const maxWidth = 140;
-  const maxHeight = 70;
-  const distanceMargin = 18;
 
   doc.rect(0, 0, doc.page.width, doc.page.height).fill("#fff");
   doc.fontSize(10);
 
-  doc
-    .fillAndStroke("#0e8cc3")
-    .lineWidth(20)
-    .lineJoin("round")
-    .rect(
-      distanceMargin,
-      distanceMargin,
-      doc.page.width - distanceMargin * 2,
-      doc.page.height - distanceMargin * 2
-    )
-    .stroke();
+  // SVGtoPDF(doc, headerGraphic, 0, 0, {
+  //   width: doc.page.width,
+  //   assumePt: true,
+  // });
 
-  doc.image(basePath + "/winners.png", doc.page.width / 2 - maxWidth / 2, 60, {
-    fit: [maxWidth, maxHeight],
-    align: "center",
-  });
-
-  jumpLine(doc, 5);
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Super Course for Awesomes", {
-      align: "center",
-    });
-
-  jumpLine(doc, 2);
-
-  // Content
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Regular.otf")
-    .fontSize(16)
-    .fill("#021c27")
-    .text("CERTIFICATE OF COMPLETION", {
-      align: "center",
-    });
-
-  jumpLine(doc, 1);
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Present to", {
-      align: "center",
-    });
-
-  jumpLine(doc, 2);
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Bold.otf")
-    .fontSize(24)
-    .fill("#021c27")
-    .text(name, {
-      align: "center",
-    });
-
-  jumpLine(doc, 1);
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Successfully completed the Super Course for Awesomes.", {
-      align: "center",
-    });
+  doc.image(basePath + "/header-graphic.png", 0, 16, { width: doc.page.width });
 
   jumpLine(doc, 7);
 
-  doc.lineWidth(1);
+  doc
+    .font(basePath + "/fonts/BarlowCondensed-Medium.ttf")
+    .fontSize(62.53)
+    .fill("#383c57")
+    .text("CERTIFICATE", {
+      align: "left",
+    });
 
-  // Signatures
-  const lineSize = 174;
-  const signatureHeight = 390;
+  doc
+    .font(basePath + "/fonts/BarlowCondensed-Medium.ttf")
+    .fontSize(25.01)
+    .fill("#e2c493")
+    .text("OF ACHIEVEMENT", {
+      align: "left",
+    });
+
+  jumpLine(doc, 1);
+
+  // Content
+  doc
+    .font(basePath + "/fonts/ABeeZee-Regular.ttf")
+    .fontSize(18)
+    .fill("#383c57")
+    .text("THIS CERTIFICATE IS PRESENTED TO", {
+      align: "left",
+    });
+
+  jumpLine(doc, 1);
+
+  doc
+    .font(basePath + "/fonts/ScopeOne-Regular.ttf")
+    .fontSize(50.03)
+    .fill("#383c57")
+    .text(name, {
+      align: "left",
+    });
+
+  const textMaxWidth = 650;
+
+  // name underline
+  let nameLineSize = 530;
+  let nameHeight = 405;
 
   doc.fillAndStroke("#021c27");
-  doc.strokeOpacity(0.2);
+  doc.strokeOpacity(0.5);
 
-  const startLine1 = 128;
-  const endLine1 = 128 + lineSize;
-  doc
-    .moveTo(startLine1, signatureHeight)
-    .lineTo(endLine1, signatureHeight)
-    .stroke();
+  let nameLine = 70;
+  let nameEndLine = nameLineSize;
+  doc.moveTo(nameLine, nameHeight).lineTo(nameEndLine, nameHeight).stroke();
 
-  const startLine2 = endLine1 + 32;
-  const endLine2 = startLine2 + lineSize;
-  doc
-    .moveTo(startLine2, signatureHeight)
-    .lineTo(endLine2, signatureHeight)
-    .stroke();
-
-  const startLine3 = endLine2 + 32;
-  const endLine3 = startLine3 + lineSize;
-  doc
-    .moveTo(startLine3, signatureHeight)
-    .lineTo(endLine3, signatureHeight)
-    .stroke();
+  doc.moveDown(0.2);
 
   doc
-    .font(basePath + "/fonts/NotoSansJP-Bold.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("John Doe", startLine1, signatureHeight + 10, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
+    .font(basePath + "/fonts/ABeeZee-Regular.ttf")
+    .fontSize(18)
+    .fill("#383c57")
+    .text(
+      `in recognition of his/her efforts and achievement in completing the program. He/She scored ${percentage}% and was awarded with ${grade} grade.`,
+      {
+        width: textMaxWidth,
+        align: "left",
+      }
+    );
+
+  doc.moveDown(0.5);
+
+  nameLineSize = 80;
+  nameHeight = doc.page.height - 50;
+
+  nameLine = 70;
+  nameEndLine = 70 + nameLineSize;
+  doc.moveTo(nameLine, nameHeight).lineTo(nameEndLine, nameHeight).stroke();
+
+  doc.moveDown(1.3);
+  doc
+    .font(basePath + "/fonts/ABeeZee-Regular.ttf")
+    .fontSize(12)
+    .fill("#383c57")
+    .text("Instructor", {
+      width: textMaxWidth,
+      align: "left",
     });
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Associate Professor", startLine1, signatureHeight + 25, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
-    });
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Bold.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Student Name", startLine2, signatureHeight + 10, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
-    });
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Student", startLine2, signatureHeight + 25, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
-    });
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Bold.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Jane Doe", startLine3, signatureHeight + 10, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
-    });
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text("Director", startLine3, signatureHeight + 25, {
-      columns: 1,
-      columnGap: 0,
-      height: 40,
-      width: lineSize,
-      align: "center",
-    });
-
-  jumpLine(doc, 4);
-
-  // Validation link
-  const link = "https://validate-your-certificate.hello/validation-code-here";
-
-  const linkWidth = doc.widthOfString(link);
-  const linkHeight = doc.currentLineHeight();
-
-  doc
-    .underline(doc.page.width / 2 - linkWidth / 2, 448, linkWidth, linkHeight, {
-      color: "#021c27",
-    })
-    .link(doc.page.width / 2 - linkWidth / 2, 448, linkWidth, linkHeight, link);
-
-  doc
-    .font(basePath + "/fonts/NotoSansJP-Light.otf")
-    .fontSize(10)
-    .fill("#021c27")
-    .text(link, doc.page.width / 2 - linkWidth / 2, 448, linkWidth, linkHeight);
-
-  // Footer
-  const bottomHeight = doc.page.height - 100;
-
-  doc.image(basePath + "/qr.png", doc.page.width / 2 - 30, bottomHeight, {
-    fit: [60, 60],
-  });
 }
 
 export default createPdf;
