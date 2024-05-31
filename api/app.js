fb60a818687db7bc3f085eb34d080ba8acb7275e
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-05-31 09:46:21 +0530

    updated cors config

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ getBasepathTo("dist");
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: ["*", "http://localhost:5173"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
     optionsSuccessStatus: 204,
